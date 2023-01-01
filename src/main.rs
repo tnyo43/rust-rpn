afc89d95d6846f28420789096f6c941bfbf4e827
Tomoya Kashifuku <tnyo43@gmail.com>
2023-01-01 16:38:22 +0900

    build command line parser with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("My RPN program")
+        .version("1.0.0")
+        .author("Tomoya")
+        .about("Super awesome sample RPN calculator")
+        .arg(
+            Arg::new("formula_file")
+                .help("Formulas written in RPN")
+                .value_name("FILE")
+                .index(1)
+                .required(false)
+        )
+        .arg(
+            Arg::new("verbose")
+                .help("Sets the level of verbosity")
+                .short('v')
+                .long("verbose")
+                .required(false)
+        )
+        .get_matches();
+    
+    match matches.get_one::<String>("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    let verbose: bool = matches.contains_id("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
