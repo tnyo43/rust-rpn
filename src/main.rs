0422ca77fd907e453238a0f5cc812cbbe5ec43c3
Tomoya Kashifuku <tnyo43@gmail.com>
2023-01-01 17:01:10 +0900

    fix parser with macro format

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,23 @@
-use clap::{Arg, Command};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Tomoya Kashifuku",
+    about = "Super awesome sample RPN calculator",
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE", default_value = "default.txt")]
+    formula_file: String,
+}
 
 fn main() {
-    let matches = Command::new("My RPN program")
-        .version("1.0.0")
-        .author("Tomoya")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("formula_file")
-                .help("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false)
-        )
-        .arg(
-            Arg::new("verbose")
-                .help("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false)
-        )
-        .get_matches();
-    
-    match matches.get_one::<String>("formula_file") {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
-    }
+    let opts = Opts::parse();
 
-    let verbose: bool = matches.contains_id("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    println!("File specified: {}", opts.formula_file);
+    println!("Is verbosity specified?: {}", opts.verbose)
 }
